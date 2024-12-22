f9e1a4e5ae8a72e66a643e6421868d4648da2ac8
Lokesh <iamlokeshahire@gmail.com>
2024-12-22 17:52:41 +0530

    game

--- src/components/LandingScreen.js
+++ src/components/LandingScreen.js
@@ -1,13 +1,10 @@
-import React from "react";
-import "../App.css";
-
 function LandingScreen({ startGame }) {
   return (
     <div className="landing-screen">
       <h1 className="title">🎉 Fun Learning Game 🎉</h1>
       <div className="loading-animation">
         <div className="spinner"></div>
-        <p>Loading Fun...</p>
+        <p className="loading-text">Loading Fun...</p>
       </div>
       <button className="start-button" onClick={startGame}>
         Start Game
